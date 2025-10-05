417b4cc09d9f0ed7fd0f7b610869a7dc24d70d74
Patrick Knight <patrick@nc4pk.net>
2025-10-05 19:47:16 -0400

    Add version information and icon to EXE builds
    Add version information, generated at build time, to the EXE builds.
    Future commits will show this version information.
    
    Add the icon as a data file so that it can be used within the app, not
    just as the executable icon.

--- Deploy.py
+++ Deploy.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import os
 import shutil
 
 import PyInstaller.__main__
@@ -24,10 +25,26 @@ import PyInstaller.__main__
 shutil.rmtree('./dist/', ignore_errors=True)
 shutil.rmtree('./build/', ignore_errors=True)
 
+# Generate version file
+os.mkdir("build")
+with open(os.path.join("build", "Version.py"), "w") as verFile:
+    try:
+        import subprocess
+        out = subprocess.check_output(
+            ["git", "describe", "--tags", "--dirty"],
+            stderr=subprocess.DEVNULL)
+        appVersion = out.decode().strip()[1:] # strip off v from tag name
+        verFile.write(f"version = \"{appVersion}\"")
+    except:
+        # can't determine version, don't bundle a potentially bad one!
+        pass
+
 PyInstaller.__main__.run([
     'Main.py',
+    os.path.join("build", "Version.py"),
     '-y', # replace output directory
     '-F', # single-file distribution
     '-w', # no console window
     '-i', 'icon.png',
+    '--add-data', 'icon.png:/',
 ])
